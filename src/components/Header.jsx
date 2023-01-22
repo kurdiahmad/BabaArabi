a79b04262f570f4c2abe17aacb15ef97d296c546
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2023-01-22 21:05:18 +0400

    Add Media query for phones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,17 @@ function Header() {
   return (
     <header id="header" class="bg-gradient text-white">
     <div class="container px-4 text-center">
-        <h1 class="fw-bolder">ابحث هنا</h1>
-        <p class="lead">قريباً...</p>
-        <script 
-        async 
-        src="https://cse.google.com/cse.js?cx=b34c0eae4471c496e">
-       </script>
-       <div 
-       class="gcse-search">
+    <div class="handp">
+    <h1 class="fw-bolder">ابحث هنا</h1>
+        <p class="lead">ما الذي تبحث عنه؟</p>
+    </div>
+
+        <div class="googleBox">
+        <div 
+       class="gcse-search" autofocus>
        </div>
+        </div>
+
     </div>
 </header>
   );
