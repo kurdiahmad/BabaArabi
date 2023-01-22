a160cc4a339fe4b1e1a42a4c5e93674f16666417
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2023-01-22 14:28:48 +0400

    Add google search box

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,13 @@ function Header() {
     <div class="container px-4 text-center">
         <h1 class="fw-bolder">ابحث هنا</h1>
         <p class="lead">قريباً...</p>
+        <script 
+        async 
+        src="https://cse.google.com/cse.js?cx=b34c0eae4471c496e">
+       </script>
+       <div 
+       class="gcse-search">
+       </div>
     </div>
 </header>
   );
